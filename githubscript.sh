d52f020d9d2cd777189ed76d1555a081002bc449
Mathieu33260 <mat_dumez@hotmail.fr>
2018-09-05 11:09:15 +0200

    Update gitignire and add username

--- githubscript.sh
+++ githubscript.sh
@@ -2,16 +2,54 @@
 FOLDER=${PWD##*/};
 README="README.md";
 GITIGNORE=".gitignore";
-curl -u 'fremin33' https://api.github.com/user/repos -d "{\"name\":\"${FOLDER}\"}";
+curl -u 'Mathieu33260' https://api.github.com/user/repos -d "{\"name\":\"${FOLDER}\"}";
 git init;
-git remote add origin git@github.com:fremin33/${FOLDER}.git;
+git remote add origin git@github.com:Mathieu33260/${FOLDER}.git;
 
 if [ ! -e "${README}" ]; then
  echo "# Readme.md" >> README.md;
 fi
 
 if [ ! -e "${GITIGNORE}" ]; then
- echo "node_modules/\nbower_components/" >> .gitignore;
+ echo "# Compiled source #\
+###################\
+*.com\
+*.class\
+*.dll\
+*.exe\
+*.o\
+*.so\
+\
+# Packages #\
+############\
+# it's better to unpack these files and commit the raw source\
+# git has its own built in compression methods\
+*.7z\
+*.dmg\
+*.gz\
+*.iso\
+*.jar\
+*.rar\
+*.tar\
+*.zip\
+\
+# Logs and databases #\
+######################\
+*.log\
+*.sql\
+*.sqlite\
+\
+# OS generated files #\
+######################
+.DS_Store\
+.DS_Store?\
+._*\
+.Spotlight-V100\
+.Trashes\
+ehthumbs.db\
+Thumbs.db\
+\
+*.idea" >> .gitignore;
 fi
 
 git add .;
